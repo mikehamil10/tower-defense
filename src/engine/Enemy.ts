b86ba4249755365a3f1ef7435e504f07faa98c82
Mike Hamilton <mhamilton8@gmail.com>
2022-07-12 08:16:07 -0500

    bug: renders enemy from center

--- src/engine/Enemy.ts
+++ src/engine/Enemy.ts
@@ -3,7 +3,7 @@ import { Vector2 } from "./entities";
 export class Enemy {
   position: Vector2;
   size: Vector2;
-  private center: Vector2;
+  center: Vector2;
 
   waypointIndex: number = 0;
   waypoints: Vector2[];
@@ -22,18 +22,19 @@ export class Enemy {
   draw(context: CanvasRenderingContext2D): void {
     context.fillStyle = "#ff0000";
     context.fillRect(
-      this.position.x,
-      this.position.y,
+      this.position.x - this.size.x / 2,
+      this.position.y - this.size.y / 2,
       this.size.x,
       this.size.y
     );
   }
 
   update(): void {
-    const xDistance = this.waypoints[this.waypointIndex].x - this.center.x;
-    const yDistance = this.waypoints[this.waypointIndex].y - this.center.y;
+    const waypoint = this.waypoints[this.waypointIndex];
+    const xDistance = waypoint.x - this.position.x;
+    const yDistance = waypoint.y - this.position.y;
 
-    const distance = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+    // const distance = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
     const angle = Math.atan2(yDistance, xDistance);
 
     this.position.x += Math.cos(angle);
@@ -43,19 +44,21 @@ export class Enemy {
       y: this.position.y + this.size.y / 2,
     };
 
-    console.log(
-      `At pos (${Math.round(this.position.x)}, ${Math.round(
-        this.position.y
-      )}). Distance to next waypoint: ${Math.round(distance)}`
-    );
+    // console.log(
+    //   `At pos (${Math.round(this.position.x)}, ${Math.round(this.position.y)}).`
+    // );
 
-    if (distance < 1 && this.waypointIndex < this.waypoints.length - 1) {
+    if (
+      Math.round(this.position.x) == Math.round(waypoint.x) &&
+      Math.round(this.position.y) == Math.round(waypoint.y) &&
+      this.waypointIndex < this.waypoints.length - 1
+    ) {
       this.waypointIndex++;
-      console.log(
-        `hit a waypoint! Heading to (${Math.round(
-          this.waypoints[this.waypointIndex].x
-        )}, ${Math.round(this.waypoints[this.waypointIndex].y)})`
-      );
+      // console.log(
+      //   `hit a waypoint! Heading to (${Math.round(
+      //     this.waypoints[this.waypointIndex].x
+      //   )}, ${Math.round(this.waypoints[this.waypointIndex].y)})`
+      // );
     }
   }
 }
